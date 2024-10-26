5ed96729cc222ece9ffea013ff79f16e1ff3f5f1
Egor <orekhov.egor33@gmail.com>
2024-10-26 18:44:21 -0400

    Fixed find_faces, added find-edges part

--- main.cpp
+++ main.cpp
@@ -5,26 +5,28 @@
 
 using namespace std;
 
+using pii = pair<int, int>;
+
 const int MAXN = 1e6+10;
 
 struct Point {
     ld x, y;
-    
+
     Point() {}
     Point(ld x_, ld y_): x(x_), y(y_) {}
-    
+
     Point operator - (const Point & p) const {
         return Point(x - p.x, y - p.y);
     }
-    
+
     ld cross (const Point & p) const {
         return x * p.y - y * p.x;
     }
-    
+
     ld cross (const Point & p, const Point & q) const {
         return (p - *this).cross(q - *this);
     }
-    
+
     int half () const {
         return int(y < 0 || (y == 0 && x < 0));
     }
@@ -76,13 +78,34 @@ inline bool edge_cmp(const edge& e1, const edge&e2){
 vector<vector<int>> find_faces(vector<Point> vertices, vector<vector<int>> adj) {
     int n = vertices.size();
     vector<vector<bool>> used(n);
+    vector<int> bfs;
+    for (int i = 0; i < n; ++i) {
+        if (adj[i].size() == 1) {
+            bfs.push_back(i);
+        }
+    }
+    for (int pt = 0; pt < bfs.size(); ++pt) {
+        int u = bfs[pt];
+        int v = adj[u][0];
+        adj[u].clear();
+        for (auto &x : adj[v]) {
+            if (x == u) {
+                swap(adj[v].back(), x);
+                adj[v].pop_back();
+                break;
+            }
+        }
+        if (adj[v].size() == 1) {
+            bfs.push_back(v);
+        }
+    }
     for (int i = 0; i < n; i++) {
-        assert(adj[i].size() > 1    );
+        if (adj[i].size() == 1) continue;
         used[i].assign(adj[i].size(), 0);
         auto compare = [&](int l, int r) {
             Point pl = vertices[l] - vertices[i];
             Point pr = vertices[r] - vertices[i];
-            cout << pl.cross(pr);
+            // cout << pl.cross(pr);
             if (pl.half() != pr.half())
                 return pl.half() < pr.half();
             return pl.cross(pr) > eps;
@@ -140,7 +163,7 @@ ifstream fin("small_Minsk.txt");
 
 ld readlds(){
     string st;
-    
+
     fin >> st;
     return stold(st);
 }
@@ -166,14 +189,14 @@ void scanline(ve<Point> p, ve<ve<int> >& faces, ve<int>& cnt, ve<Point>& users){
         es.push_back({0, i.x, edge(i, i, 0)});
     sort(es.begin(), es.end());
     set<edge, decltype(*edge_cmp)> st(edge_cmp);
-    
+
     for(int i = 0; i < es.size(); i++){
         auto [t, x, e, id] = es[i];
         if(t == 1){
             int prevsz = st.size();
             st.insert(e);
 //            assert(prevsz == st.size() - 1);
-            
+
         }
         else if(t == -1){
             st.erase(e);
@@ -181,7 +204,7 @@ void scanline(ve<Point> p, ve<ve<int> >& faces, ve<int>& cnt, ve<Point>& users){
         else{
             auto it = st.upper_bound(e);
             if(it == st.begin()){
-                cout << e.l.x << " " << e.l.y << endl;
+                //cout << e.l.x << " " << e.l.y << endl;
                 continue;
             }
             it--;
@@ -189,6 +212,56 @@ void scanline(ve<Point> p, ve<ve<int> >& faces, ve<int>& cnt, ve<Point>& users){
         }
     }
 }
+
+vector<pii> init_edges(vector<vector<int>> &faces) {
+    map<pii, vector<int>> mp;
+    for (int i = 0; i < faces.size(); ++i) {
+        auto &ve = faces[i];
+        assert(ve.size() > 1);
+        for (int j = 0; j < ve.size(); ++j) {
+            int u = ve[j], v = ve[(j + 1) % ve.size()];
+            mp[minmax(u, v)].push_back(i);
+        }
+    }
+    vector<pii> edges;
+    for (auto &[_, a] : mp) {
+        sort(a.begin(), a.end());
+        a.erase(unique(a.begin(), a.end()), a.end());
+        assert(a.size() <= 2);
+        if (a.size() > 1) {
+            edges.emplace_back(a[0], a[1]);
+        }
+    }
+    sort(edges.begin(), edges.end());
+    edges.erase(unique(edges.begin(), edges.end()), edges.end());
+    return edges;
+}
+
+mt19937 rng(5);
+
+struct dsu {
+    vector<int> p;
+    dsu(int n): p(n, -1) {}
+    int get(int u) {
+        return p[u] < 0 ? u : p[u] = get(p[u]);
+    }
+    bool same(int u, int v) {
+        return get(u) == get(v);
+    }
+    void unite(int u, int v) {
+        u = get(u), v = get(v);
+        if (p[u] < p[v]) {
+            swap(u, v);
+        }
+        p[u] += p[v];
+        p[v] = u;
+    }
+    void clear() {
+        fill(p.begin(), p.end(), -1);
+    }
+};
+
+const int L = 512, R = 1024;
 
 int main(){
     int n;
@@ -212,6 +285,7 @@ int main(){
         g[b].push_back(a);
     }
     ve<ve<int> > faces = find_faces(pts, g);
+    int z = faces.size();
     int t;
     fin >> t;
     ve<Point> users(t);
@@ -223,6 +297,42 @@ int main(){
     }
     ve<int> cnt(faces.size(), 0);
     scanline(pts, faces, cnt, users);
-    for(auto i : cnt)
-        cout << i << " ";
+//    for(auto i : cnt)
+//        cout << i << " ";
+    auto edges = init_edges(faces);
+    dsu d(z);
+    vector<int> cur_cnt(z);
+    int iter = 0;
+    while (true) { // this part sucks
+        shuffle(edges.begin(), edges.end(), rng);
+        d.clear();
+        cur_cnt = cnt;
+        int cmp = z;
+        for (auto &[u, v] : edges) {
+            if (!d.same(u, v)) {
+                int cu = cur_cnt[d.get(u)];
+                int cv = cur_cnt[d.get(v)];
+                if ((cu < L || cv < L) && cu + cv <= R) {
+                    cur_cnt[d.get(u)] = cur_cnt[d.get(v)] = 0;
+                    d.unite(u, v);
+                    cur_cnt[d.get(u)] = cu + cv;
+                }
+            }
+        }
+        bool done = true;
+        int mn = 10000000, mx = 0;
+        for (int i = 0; i < z; ++i) {
+            if (cur_cnt[d.get(i)] < L || cur_cnt[d.get(i)] > R) {
+                done = false;
+            }
+            if (cur_cnt[d.get(i)] > 0) {
+                mn = min(mn, cur_cnt[d.get(i)]);
+            }
+            mx = max(mx, cur_cnt[d.get(i)]);
+        }
+        if (done) break;
+        cout << iter++ << ": " << mn << ", " << mx << "\n";
+    }
+    cout << "yay\n";
+    return 0;
 }
